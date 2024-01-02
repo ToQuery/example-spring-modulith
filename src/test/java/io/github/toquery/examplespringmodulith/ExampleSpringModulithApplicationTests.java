b41aa8fccc30aa2f53f8a989f1b8c9a416bffafb
ToQuery <toquery@qq.com>
2024-01-02 17:50:11 +0800

    first commit

---
+++ src/test/java/io/github/toquery/examplespringmodulith/ExampleSpringModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.toquery.examplespringmodulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
